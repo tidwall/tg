757a35494f7693c39d91355ae40b8339f3168418
Dewey Dunnington <dewey@dunnington.ca>
2026-09-04 17:29:38 -0500

    ci: test native MSVC compatibility

--- tg.c
+++ tg.c
@@ -4,6 +4,8 @@
 // Use of this source code is governed by an MIT-style
 // license that can be found in the LICENSE file.
 
+#define _USE_MATH_DEFINES
+
 #include <math.h>
 #include <float.h>
 #include <stdarg.h>
@@ -16,6 +18,15 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <stddef.h>
+
+#if defined(_MSC_VER)
+#undef __BYTE_ORDER__
+#undef __ORDER_LITTLE_ENDIAN__
+#undef __ORDER_BIG_ENDIAN__
+#define __BYTE_ORDER__ 1
+#define __ORDER_LITTLE_ENDIAN__ 1
+#define __ORDER_BIG_ENDIAN__ 0
+#endif
 
 /******************************************************************************
 
@@ -562,10 +573,22 @@ static void rc_init(rc_t *rc) {
     atomic_init(rc, 0);
 }
 static void rc_retain(rc_t *rc) {
+#if defined(_MSC_VER)
+    (void)atomic_fetch_add(rc, 1);
+#else
     atomic_fetch_add_explicit(rc, 1, __ATOMIC_RELAXED);
+#endif
 }
 static bool rc_release(rc_t *rc) {
+#if defined(_MSC_VER)
+    if (atomic_fetch_sub(rc, 1) == 1) {
+        atomic_thread_fence(memory_order_acquire);
+        return true;
+    }
+    return false;
+#else
     return atomic_fetch_sub_explicit(rc, 1, __ATOMIC_ACQ_REL) == 1;
+#endif
 }
 
 #endif
@@ -13283,17 +13306,29 @@ static const char *wkb_invalid_child_type(void) {
     return "invalid child type";
 }
 
+#if defined(_MSC_VER)
+#include <stdlib.h>
+#define bswap32 _byteswap_ulong
+#define bswap64 _byteswap_uint64
+#else
+#define bswap32 __builtin_bswap32
+#define bswap64 __builtin_bswap64
+#endif
+
 static uint32_t read_uint32(const uint8_t *data, bool swap) {
     uint32_t x;
     memcpy(&x, data, sizeof(uint32_t));
-    return swap ? __builtin_bswap32(x) : x;
+    return swap ? bswap32(x) : x;
 }
 
 static uint64_t read_uint64(const uint8_t *data, bool swap) {
     uint64_t x;
     memcpy(&x, data, sizeof(uint64_t));
-    return swap ? __builtin_bswap64(x) : x;
+    return swap ? bswap64(x) : x;
 }
+
+#undef bswap32
+#undef bswap64
 
 static double read_double(const uint8_t *data, bool swap) {
     return ((union raw_double){.u=read_uint64(data, swap)}).d;
@@ -14499,7 +14534,7 @@ static size_t parse_geobin(const uint8_t *geobin, size_t len, size_t i,
 
 static struct tg_geom *parse_hex(const char *hex, size_t len, enum tg_index ix)
 {
-    const uint8_t _ = 0;
+#define _ 0
     static const uint8_t hextoks[256] = { 
         _,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,
         _,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,01,2,3,4,5,6,7,8,9,10,_,_,_,_,_,
@@ -14538,6 +14573,7 @@ static struct tg_geom *parse_hex(const char *hex, size_t len, enum tg_index ix)
     return geom;
 invalid:
     if (must_free) tg_free(dst);
+#undef _
     return make_parse_error(wkb_invalid_err());
 }
 
